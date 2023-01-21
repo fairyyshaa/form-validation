334d55f29c1167e73784743ff8d0ca6165a9743f
fairyyshaa <fairyyshaa97@gmail.com>
2023-01-21 19:35:00 +0100

    create the project

---
+++ vanil.js
@@ -0,0 +1,9 @@
+let firsName = document.getElementById("firstname");
+let lastName = document.getElementById("lastname");
+let telephone = document.getElementById("telephone");
+let email= document.getElementById("email");
+
+console.log(firsName);
+console.log(lastName);
+console.log(telephone);
+console.log(email);
